b3824580d30749e0b63b3e23e0745d3a8872f64c
rafasz <43715742+rafasz@users.noreply.github.com>
2021-08-09 08:20:57 -0300

    Initial commit

---
+++ src/main/java/com/example/helloci/HellociApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellociApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellociApplication.class, args);
+	}
+
+}
